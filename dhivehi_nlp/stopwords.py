8cf14a6a972235ccaed2d9caaef876ad47b9a43a
mismaah <mismaahabdulla@gmail.com>
2021-02-12 16:54:31 +0500

    create stopword module (stopword list incomplete)

---
+++ dhivehi_nlp/stopwords.py
@@ -0,0 +1,30 @@
+"""Remove stopwords from text and return the resulting tokens.
+  ލިޔުމުގައިވާ ބަސްތަކުގެ ތެރެއިން ލިޔުމުގެ މާނައަށް ބަދަލުނުގެންނަ ބަސްތައް ނެގުމަށްފަހު ބާކީހުރި ބަސްތައް އަނބުރާދިނުން
+"""
+
+import tokenizer
+
+def get_stopwords():
+    """
+    Returns a list of stopwords.
+                                                    ސްޓޮޕްވާޑް ތަކުގެ ލިސްޓެއް އަނބުރާދޭނެއެވެ
+    """
+    return [
+        "އަދި",
+        "",
+        "",
+        "",
+    ]
+
+def remove_stopwords(text):
+    """
+    Returns a list of tokens from the input text after removing the stopwords.
+                         ލިޔުމުގައިވާ ސްޓޮޕްވާޑް ނެގުމަށްފަހު ބާކީހުރި ބަސްތައް ލިސްޓެއް ގޮތަށް އަނބުރާދޭނެއެވެ
+    """
+    tokens = tokenizer.word_tokenize(text)
+    stopwords = get_stopwords()
+    results = []
+    for token in tokens:
+        if token not in stopwords:
+            results.append(token)
+    return results
