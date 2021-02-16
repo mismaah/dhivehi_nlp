a1defeeeb8e3be4a32fecec02b376968ecf93deb
mismaah <mismaahabdulla@gmail.com>
2021-02-16 07:37:45 +0500

    changes to stopwords module
    added stopwords list (still can be expanded)
    remove punctuation before removing stopwords
    add new test case

--- dhivehi_nlp/stopwords.py
+++ dhivehi_nlp/stopwords.py
@@ -12,6 +12,40 @@ def get_stopwords():
     """
     return [
         "އަދި",
+        "ދަށުން",
+        "އެވެ",
+        "ވަނަ",
+        "ވަނީ",
+        "މިވަނީ",
+        "އެ",
+        "މި",
+        "އާއި",
+        "އިން",
+        "ވެސް",
+        "ކަމަށް",
+        "ނަމަ",
+        "ވާތީ",
+        "ވުރެ",
+        "ހުރި",
+        "ހުންނެވި",
+        "ކޮށް",
+        "އިރު",
+        "ދެން",
+        "ހަމަ",
+        "އެކު",
+        "މީގެ",
+        "ފަހު",
+        "ފަހުން",
+        "ފަހުގެ",
+        "ކުރި",
+        "ކުރިން",
+        "ކުރީގެ",
+        "އެގޮތުން",
+        "ބެހޭ",
+        "އަށް",
+        "ވަރަށް",
+        "ކަމުން",
+        "ކަމުގައި",
     ]
 
 
@@ -20,7 +54,7 @@ def remove_stopwords(text):
     Returns a list of tokens from the input text after removing the stopwords.
                          ލިޔުމުގައިވާ ސްޓޮޕްވާޑް ނެގުމަށްފަހު ބާކީހުރި ބަސްތައް ލިސްޓެއް ގޮތަށް އަނބުރާދޭނެއެވެ
     """
-    tokens = tokenizer.word_tokenize(text)
+    tokens = tokenizer.word_tokenize(text, removePunctuation=True)
     stopwords = get_stopwords()
     results = []
     for token in tokens:
