965365b39275afe45a828ddf1cf65becfe6e6f16
mismaah <mismaahabdulla@gmail.com>
2021-02-18 19:21:52 +0500

    language models module
    change ngram function to return as dict along with count
    create model function which gives probability of ngrams
    modify old unit test to fit new ngram function
    create new unit tests

--- dhivehi_nlp/language_models.py
+++ dhivehi_nlp/language_models.py
@@ -2,13 +2,22 @@ from dhivehi_nlp import tokenizer
 
 
 def ngrams(text, n):
-    sentences = tokenizer.sentence_tokenize(text)
-    grams = []
-    for sentence in sentences:
-        tokens = tokenizer.word_tokenize(sentence)
-        grams_sentence = [tuple(tokens[i : i + n]) for i in range(len(tokens) - n + 1)]
-        grams = grams + grams_sentence
-    return sorted(list(set(grams)))
+    if n == 1:
+        grams = tokenizer.word_tokenize(text, removePunctuation=True)
+    else:
+        sentences = tokenizer.sentence_tokenize(text)
+        grams = []
+        for sentence in sentences:
+            tokens = tokenizer.word_tokenize(sentence, removePunctuation=True)
+            grams_sentence = [
+                tuple(tokens[i : i + n]) for i in range(len(tokens) - n + 1)
+            ]
+            grams = grams + grams_sentence
+    grams_counted = []
+    for gram in set(grams):
+        counted = {"gram": gram, "count": grams.count(gram)}
+        grams_counted.append(counted)
+    return grams_counted
 
 
 def unigrams(text):
@@ -17,3 +26,29 @@ def unigrams(text):
 
 def bigrams(text):
     return ngrams(text, 2)
+
+
+def model(text, n):
+    probabilities = []
+    if n == 1:
+        grams = ngrams(text, 1)
+        tokens = tokenizer.word_tokenize(text)
+        for i in grams:
+            probability = {"gram": i["gram"], "probability": i["count"] / len(tokens)}
+            probabilities.append(probability)
+    else:
+        grams = ngrams(text, n)
+        grams_minus = ngrams(text, n - 1)
+        for gram in grams:
+            if n == 2:
+                gram_search = gram["gram"][0]
+            else:
+                gram_search = gram["gram"][: n - 1]
+            for g in grams_minus:
+                if g["gram"] == gram_search:
+                    probability = {
+                        "gram": gram["gram"],
+                        "probability": gram["count"] / g["count"],
+                    }
+                    probabilities.append(probability)
+    return probabilities
--- test/test_language_models.py
+++ test/test_language_models.py
@@ -5,22 +5,78 @@ from dhivehi_nlp import language_models
 class test_tokenizer(unittest.TestCase):
     def test_ngrams_one_sentence(self):
         text = ".ބުނެފައި އަދި އިތުރު"
-        self.assertEqual(
-            language_models.ngrams(text, 2), [("ބުނެފައި", "އަދި"), ("އަދި", "އިތުރު")]
+        self.assertCountEqual(
+            language_models.ngrams(text, 2),
+            [
+                {"gram": ("ބުނެފައި", "އަދި"), "count": 1},
+                {"gram": ("އަދި", "އިތުރު"), "count": 1},
+            ],
         )
 
     def test_ngrams_multiple_sentences_0(self):
         text = "ބުނެފައި އަދި އިތުރު. ބުނެފައި އަދި އިތުރުކަމެއް"
-        self.assertEqual(
+        self.assertCountEqual(
             language_models.ngrams(text, 2),
-            [("ބުނެފައި", "އަދި"), ("އަދި", "އިތުރު"), ("އަދި", "އިތުރުކަމެއް")],
+            [
+                {"gram": ("ބުނެފައި", "އަދި"), "count": 2},
+                {"gram": ("އަދި", "އިތުރު"), "count": 1},
+                {"gram": ("އަދި", "އިތުރުކަމެއް"), "count": 1},
+            ],
         )
 
     def test_ngrams_multiple_sentences_1(self):
         text = "ބުނެފައި އަދި އިތުރު. ބުނެފައި އަދި އިތުރުކަމެއް"
-        self.assertEqual(
+        self.assertCountEqual(
             language_models.ngrams(text, 3),
-            [("ބުނެފައި", "އަދި", "އިތުރު"), ("ބުނެފައި", "އަދި", "އިތުރުކަމެއް")],
+            [
+                {"gram": ("ބުނެފައި", "އަދި", "އިތުރު"), "count": 1},
+                {"gram": ("ބުނެފައި", "އަދި", "އިތުރުކަމެއް"), "count": 1},
+            ],
+        )
+
+    def test_ngrams_multiple_sentences_2(self):
+        text = "ބުނެފައި އަދި އިތުރު. ބުނެފައި އަދި އިތުރުކަމެއް"
+        self.assertCountEqual(
+            language_models.ngrams(text, 1),
+            [
+                {"gram": "ބުނެފައި", "count": 2},
+                {"gram": "އަދި", "count": 2},
+                {"gram": "އިތުރު", "count": 1},
+                {"gram": "އިތުރުކަމެއް", "count": 1},
+            ],
+        )
+
+    def test_model_unigram_prob(self):
+        text = "ބުނެފައި އަދި އިތުރު. ބުނެފައި އަދި އިތުރުކަމެއް"
+        self.assertCountEqual(
+            language_models.model(text, 1),
+            [
+                {"gram": "ބުނެފައި", "probability": 0.3333333333333333},
+                {"gram": "އަދި", "probability": 0.3333333333333333},
+                {"gram": "އިތުރު", "probability": 0.16666666666666666},
+                {"gram": "އިތުރުކަމެއް", "probability": 0.16666666666666666},
+            ],
+        )
+
+    def test_model_bigram_prob(self):
+        text = "ބުނެފައި އަދި އިތުރު. ބުނެފައި އަދި އިތުރުކަމެއް"
+        self.assertCountEqual(
+            language_models.model(text, 2),
+            [
+                {"gram": ("ބުނެފައި", "އަދި"), "probability": 1},
+                {"gram": ("އަދި", "އިތުރު"), "probability": 0.5},
+                {"gram": ("އަދި", "އިތުރުކަމެއް"), "probability": 0.5},
+            ],
+        )
+
+    def test_model_trigram_prob(self):
+        text = "ބުނެފައި އަދި އިތުރު. ބުނެފައި އަދި އިތުރުކަމެއް"
+        self.assertCountEqual(
+            language_models.model(text, 3),
+            [
+                {"gram": ("ބުނެފައި", "އަދި", "އިތުރު"), "probability": 0.5},
+                {"gram": ("ބުނެފައި", "އަދި", "އިތުރުކަމެއް"), "probability": 0.5},
+            ],
         )
 
 
