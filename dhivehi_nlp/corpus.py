958d5617f7ed4a47b48d6df9d97e3ff5b231ce67
mismaah <mismaahabdulla@gmail.com>
2021-02-20 21:35:48 +0500

    corpus module
    insert corpus text into db corpus table
    create corpus module
    create unit tests

---
+++ dhivehi_nlp/corpus.py
@@ -0,0 +1,62 @@
+"""Collections of various Dhivehi texts.
+
+                                                              އެކި ބާވަތުގެ ދިވެހި ލިޔުންތައް
+"""
+
+import sqlite3
+
+
+def db_connect(db_path="./store/dhivehi_nlp.db"):
+    con = sqlite3.connect(db_path)
+    return con
+
+
+def db_read(name):
+    con = db_connect()
+    cursor = con.cursor()
+    query = f"SELECT content FROM corpus WHERE name='{name}'"
+    cursor.execute(query)
+    content = cursor.fetchone()
+    con.close()
+    return content[0]
+
+
+def news():
+    """News article from sun.mv https://sun.mv/145845
+
+                                                             ސަން އެމްވީގެ ނޫސް ހަބަރެއް
+    """
+    return db_read("news")
+
+
+def story():
+    """Rehendhi short story by Aishath Afra https://vaahaka.com/story/9604/
+
+                                                         އައިޝަތު އަފްރާގެ ކުރުވާހަކަ ރެހެނދި
+    """
+    return db_read("story")
+
+
+def tweet():
+    """Tweet by @FSaaira https://twitter.com/FSaaira/status/1334579523006812160
+
+                                                                           ޓްވީޓެއް
+    """
+    return db_read("tweet")
+
+
+def lyrics():
+    """Lyrics of Hingaa Hoadhama Hey by Hussain Ali
+
+                                                     ހުސެން އަލީގެ ހިނގާ ހޯދަމާހޭ ލަވައިގެ ޅެން
+    """
+    return db_read("lyrics")
+
+
+def textbook():
+    """Excerpt from Dhivehi 3 (Sh) teachers book 
+    https://www.moe.gov.mv/en/category/view/31
+
+                                                 ދިވެހި 3 (ށ) މުދައްރިސުންގެ ފޮތުން ތަންކޮޅެއް
+    """
+    return db_read("textbook")
